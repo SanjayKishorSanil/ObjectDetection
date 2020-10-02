a6669170bfc7a503784de625b6f61136d549f4c1
SanjayKishorSanil <52646889+SanjayKishorSanil@users.noreply.github.com>
2020-10-02 17:15:08 +0530

    Add files via upload

---
+++ ObjectDetection/ReadmeObjectDetection.py
@@ -0,0 +1,24 @@
+1) Download and install Python 3.6 from official Python Language website
+https://www.python.org/downloads/release/python-360/
+2) Install the following dependencies via pip:
+i. TensorFlow
+pip3 install tensorflow==1.13.1
+ii. OpenCV
+pip3 install opencv-python
+iii. Keras
+pip3 install keras==2.2.4
+iv. Numpy
+pip3 install numpy==1.16.1
+v. ImageAI
+pip3 install imageai --upgrade
+3) Download the RetinaNet model file that will be used for object detection via this link.
+https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/resnet50_coco_best_v2.0.1.h5
+ Copy the RetinaNet model file and the image you want to detect to the folder that contains the python file.
+Then run the code and wait while the results prints in the console. Once the result is printed to the console, go to the folder in which your Object
+Detection.py is and you will find a new image saved.
+
+
+
+
+
+ 
